ecf5d6745140a5a5ac2ef03eb7023cf7165e5a02
dhawan-vaibhav <dhawan-vaibhav@github.com>
2023-02-14 19:31:46 -0500

    made deploy changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import PathFindingVisualizer from "./pathFindingVisualizer.jsx";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export function MyVerticallyCenteredModal(props) {
   return (
@@ -17,17 +15,22 @@ export function MyVerticallyCenteredModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-            Dijkstra Path Finding Visualizer
+          Dijkstra Path Finding Visualizer
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <h4>Tutorial</h4>
-       <ol>
-         <li>Press and drag start (RED) and end (Green) nodes to change their positions</li>
-         <li>Press and drag to create walls </li>
-         <li>Press Visualize! button</li>
-       </ol>
-       <p style={{color:"red"}}>Note: This project is still being worked on and not complete.</p>
+        <ol>
+          <li>
+            Press and drag start (RED) and end (Green) nodes to change their
+            positions
+          </li>
+          <li>Press and drag to create walls </li>
+          <li>Press Visualize! button</li>
+        </ol>
+        <p style={{ color: "red" }}>
+          Note: This project is still being worked on and not complete.
+        </p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -36,13 +39,15 @@ export function MyVerticallyCenteredModal(props) {
   );
 }
 function App() {
-
   const [modalShow, setModalShow] = React.useState(true);
   return (
-   <div>
-     <MyVerticallyCenteredModal show={modalShow}onHide={() => setModalShow(false)}/>
-     <PathFindingVisualizer/>
-   </div>
+    <div>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <PathFindingVisualizer />
+    </div>
   );
 }
 
